46dbe79c1b11fd806f63a162c4b27b0e72bf0632
lightnear <lightnear@qq.com>
2022-03-07 16:24:56 +0800

    fix: fix apt-get ca-certificates error

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,18 @@
 
 All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
 
+## [1.2.0](https://github.com/lightnear/dockerfiles/compare/v1.1.0...v1.2.0) (2022-03-07)
+
+
+### Features
+
+* add chevereto docker image ([097457f](https://github.com/lightnear/dockerfiles/commit/097457f3fac6b3a9372ebfff0179a7fdef013d19))
+
+
+### Bug Fixes
+
+* fix apt-get ca-certificates error ([cf0ff71](https://github.com/lightnear/dockerfiles/commit/cf0ff71e15972337498e53c852d1591f5540cf9e))
+
 ## 1.1.0 (2022-02-23)
 
 
---
+++ chevereto/docker-entrypoint.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -Eeuo pipefail
+
+# Create missing directory if we don't have and give permission
+[[ ! -d /var/www/html/ ]] && mkdir -p /var/www/html/
+chown -R www-data:www-data /var/www/html
+
+# Download & install chevereto if there is no index.php (it means already be installed chevereto)
+if [[ ! -f /var/www/html/index.php ]]; then
+  if [ -z ${CHEVERETO_RELEASE+x} ]; then
+    CHEVERETO_RELEASE=$(curl -sX GET "https://api.github.com/repos/rodber/chevereto-free/releases/latest" \
+    | awk '/tag_name/{print $4;exit}' FS='[""]')
+  fi
+  curl -o /tmp/chevereto.tar.gz -L "https://github.com/rodber/chevereto-free/archive/${CHEVERETO_RELEASE}.tar.gz"
+  tar xf /tmp/chevereto.tar.gz -C /var/www/html --strip-components=1
+  chown -R www-data:www-data /var/www/html
+  mv /var/www/html/sync.sh /tmp/sync.sh
+  composer install \
+    --working-dir=/var/www/html \
+    --prefer-dist \
+    --no-progress \
+    --classmap-authoritative \
+    --ignore-platform-reqs
+  mv /tmp/sync.sh /var/www/sync.sh
+  chmod +x /var/www/sync.sh
+fi
+# [[ -f /var/www/html/installer.php ]] && chown www-data:www-data /var/www/html/installer.php
+
+# Run apache2 foreground
+exec "$@"
